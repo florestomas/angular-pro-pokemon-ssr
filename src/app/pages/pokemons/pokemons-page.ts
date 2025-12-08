15cc1d3c4426565e85bbd59b599c610cdc14e1fc
florestomas <floresmancillatomas@gmail.com>
2025-12-08 19:02:52 -0300

    ApplictionRef - STABLE

--- src/app/pages/pokemons/pokemons-page.ts
+++ src/app/pages/pokemons/pokemons-page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { ApplicationRef, Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { PokemonList } from '../../pokemons/components/pokemon-list/pokemon-list';
 import { PokemonListSkeleton } from './ui/pokemon-list-skeleton/pokemon-list-skeleton';
 
@@ -7,12 +7,23 @@ import { PokemonListSkeleton } from './ui/pokemon-list-skeleton/pokemon-list-ske
   imports: [PokemonListSkeleton, PokemonList],
   templateUrl: './pokemons-page.html',
 })
-export default class PokemonsPage implements OnInit {
-  public isLoading = signal(true);
+export default class PokemonsPage implements OnInit /* , OnDestroy */ {
+  /*   public isLoading = signal(true);
+
+  private appRef = inject(ApplicationRef);
+
+  private $appState = this.appRef.isStable.subscribe((isStable) => {
+    console.log({ isStable });
+  }); */
 
   ngOnInit(): void {
-    setTimeout(() => {
+    /* setTimeout(() => {
       this.isLoading.set(false);
-    }, 1500);
+    }, 1500); */
   }
+  /*
+  ngOnDestroy(): void {
+    console.log('destroy');
+    this.$appState.unsubscribe();
+  } */
 }
