d4baf6a9709b6624e967b01abd4c1a2094410bbb
florestomas <floresmancillatomas@gmail.com>
2025-12-09 17:36:33 -0300

    Realizar peticiones HTTP

--- src/app/pages/pokemons/pokemons-page.ts
+++ src/app/pages/pokemons/pokemons-page.ts
@@ -1,6 +1,7 @@
 import { ApplicationRef, Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { PokemonList } from '../../pokemons/components/pokemon-list/pokemon-list';
 import { PokemonListSkeleton } from './ui/pokemon-list-skeleton/pokemon-list-skeleton';
+import { Pokemons } from '../../pokemons/services/pokemons';
 
 @Component({
   selector: 'pokemons-page',
@@ -8,22 +9,28 @@ import { PokemonListSkeleton } from './ui/pokemon-list-skeleton/pokemon-list-ske
   templateUrl: './pokemons-page.html',
 })
 export default class PokemonsPage implements OnInit /* , OnDestroy */ {
-  /*   public isLoading = signal(true);
+  public isLoading = signal(true);
 
-  private appRef = inject(ApplicationRef);
+  private pokemonsService = inject(Pokemons);
+
+  /*   private appRef = inject(ApplicationRef);
 
   private $appState = this.appRef.isStable.subscribe((isStable) => {
     console.log({ isStable });
-  }); */
+  });  */
 
   ngOnInit(): void {
-    /* setTimeout(() => {
-      this.isLoading.set(false);
-    }, 1500); */
+    this.loadPokemons();
   }
   /*
   ngOnDestroy(): void {
     console.log('destroy');
     this.$appState.unsubscribe();
   } */
+
+  public loadPokemons(page = 0) {
+    this.pokemonsService.loadpage(page).subscribe((pokemons) => {
+      console.log('On Init');
+    });
+  }
 }
---
+++ src/app/pokemons/services/pokemons.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { map, Observable, tap } from 'rxjs';
+import { PokeAPIResponse, SimplePokemon } from '../interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Pokemons {
+  private http = inject(HttpClient);
+
+  public loadpage(page: number): Observable<SimplePokemon[]> {
+    if (page !== 0) {
+      --page; // 1 = 0
+    }
+
+    page = Math.max(0, page);
+
+    return this.http
+      .get<PokeAPIResponse>(`https://pokeapi.co/api/v2/pokemon?offset=${page * 20}&limit=20`)
+      .pipe(
+        map((resp) => {
+          const simplePokemons: SimplePokemon[] = resp.results.map((pokemon) => ({
+            id: pokemon.url.split('/').at(-2) ?? '',
+            name: pokemon.name,
+          }));
+
+          return simplePokemons;
+        }),
+        tap((pokemons) => console.log(pokemons))
+      );
+  }
+}
